0d7a3ef5c433ee12f5362f2bc30fa88ef8d93235
Dmitry Kukolev <89936000+JSilver1979@users.noreply.github.com>
2022-10-12 15:36:51 +0300

    Update ProductService.java

--- src/main/java/ru/gb/jSilver/SpringMarket/services/ProductService.java
+++ src/main/java/ru/gb/jSilver/SpringMarket/services/ProductService.java
@@ -3,14 +3,14 @@ package ru.gb.jSilver.SpringMarket.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.gb.jSilver.SpringMarket.dto.Product;
-import ru.gb.jSilver.SpringMarket.repos.ProductRepo;
+import ru.gb.jSilver.SpringMarket.repos.ProductRepository;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-    private final ProductRepo repo;
+    private final ProductRepo productRepository;
 
     public Product getProductByID (Integer id) {
         return repo.findByID(id);
